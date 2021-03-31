d32e441e8b394d7e8b9e27ab1068be1ff00ed1db
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-03-31 17:45:29 -0300

    Permissionless breaker (#3)
    * Initial setup for emergency circuit breaker if ilk price drop is out of acceptable bounds
    
    * Remove price conversion to dai & unnecessary ilk_ param to e-break
    
    * Minor changes + some tests
    
    * locked per ilk
    
    * Rename function
    
    * Minor changes
    
    * Allow up to level number 3
    
    * Variables renaming
    
    * Make tripBreaker set the level to 2
    
    * Add comment
    
    * Define price tolerance as the percentage of the actual price
    
    * Add a test of stopping multiple clippers (#4)
    
    * Fix test after merging
    
    * Update src/ClipperMom.sol
    
    Co-authored-by: Kurt Barry <kurt@makerdao.com>
    
    * Changes on comment
    
    * Require the clipper to be set below 2 to execute tripBreaker
    
    * tolerance and locked mappings with clipper key
    
    * Use osm.hop() instead of harcoded time
    
    * Add comment to setPriceTolerance function
    
    * minor change to comment
    
    * Change error message
    
    * Update src/ClipperMom.sol
    
    Co-authored-by: Kurt Barry <kurt@makerdao.com>
    
    * Set delay via param in SetBreaker
    
    * Use safe add
    
    * Remove unused safe sub
    
    * Add comment
    
    * Remove tolerance 0 check
    
    * Add comment
    
    Co-authored-by: Tannr <tannr@hey.com>
    Co-authored-by: Brian L. McMichael <brian@brianmcmichael.com>
    Co-authored-by: Kurt Barry <kurt@makerdao.com>

--- src/ClipperMom.t.sol
+++ src/ClipperMom.t.sol
@@ -23,6 +23,18 @@ import "ds-test/test.sol";
 
 import "./ClipperMom.sol";
 
+contract Anyone {
+    ClipperMom mom;
+
+    constructor(ClipperMom mom_) public {
+        mom = mom_;
+    }
+
+    function tripBreaker(address clip_) external {
+        mom.tripBreaker(clip_);
+    }
+}
+
 contract MomCaller {
     ClipperMom mom;
 
@@ -30,16 +42,20 @@ contract MomCaller {
         mom = mom_;
     }
 
-    function setOwner(address newOwner) public {
+    function setOwner(address newOwner) external {
         mom.setOwner(newOwner);
     }
 
-    function setAuthority(address newAuthority) public {
+    function setAuthority(address newAuthority) external {
         mom.setAuthority(newAuthority);
     }
 
-    function setBreaker(address clip, uint256 level) public {
-        mom.setBreaker(clip, level);
+    function setBreaker(address clip_, uint256 level, uint256 delay) external {
+        mom.setBreaker(clip_, level, delay);
+    }
+
+    function setPriceTolerance(address clip, uint256 value) external {
+        mom.setPriceTolerance(address(clip), value);
     }
 }
 
@@ -65,8 +81,10 @@ contract MockClipper {
         require(wards[msg.sender] == 1, "MockClipper/not-authorized");
         _;
     }
-    constructor() public {
+    bytes32 public ilk;
+    constructor(bytes32 _ilk) public {
         wards[msg.sender] = 1;
+        ilk = _ilk;
     }
     function file(bytes32 what, uint256 data) external auth {
         if (what == "stopped") stopped = data;
@@ -74,19 +92,80 @@ contract MockClipper {
     }
 }
 
+contract MockSpotter {
+    address public osm;
+
+    constructor(address osm_) public {
+        osm = osm_;
+    }
+
+    function ilks(bytes32) external view returns (address osm_, uint256) {
+        osm_ = osm;
+    }
+}
+
+contract MockOsm {
+    struct Feed {
+        uint128 val;
+        uint128 has;
+    }
+
+    Feed cur;
+    Feed nxt;
+
+    function setCurPrice(uint256 val, uint256 has) external {
+        cur.val = uint128(val);
+        cur.has = uint128(has);
+    }
+
+    function setNxtPrice(uint256 val, uint256 has) external {
+        nxt.val = uint128(val);
+        nxt.has = uint128(has);
+    }
+
+    function peek() external view returns (bytes32,bool) {
+        return (bytes32(uint256(cur.val)), cur.has == 1);
+    }
+
+    function peep() external view returns (bytes32,bool) {
+        return (bytes32(uint256(nxt.val)), nxt.has == 1);
+    }
+}
+
+interface Hevm {
+    function warp(uint256) external;
+}
+
 contract ClipperMomTest is DSTest {
+    MockOsm osm;
+    MockSpotter spotter;
     ClipperMom mom;
     MomCaller caller;
     SimpleAuthority authority;
     MockClipper clip;
+    Anyone anyone;
+
+    Hevm hevm;
+
+    uint256 constant WAD = 10 ** 18;
+    uint256 constant RAY = 10 ** 27;
+
+    // CHEAT_CODE = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+    bytes20 constant CHEAT_CODE =
+        bytes20(uint160(uint256(keccak256('hevm cheat code'))));
 
     function setUp() public {
-        mom = new ClipperMom();
+        hevm = Hevm(address(CHEAT_CODE));
+        osm = new MockOsm();
+        spotter = new MockSpotter(address(osm));
+        mom = new ClipperMom(address(spotter));
         caller = new MomCaller(mom);
         authority = new SimpleAuthority(address(caller));
         mom.setAuthority(address(authority));
-        clip = new MockClipper();
+        clip = new MockClipper("ETH");
         clip.rely(address(mom));
+        anyone = new Anyone(mom);
+        hevm.warp(1000);
     }
 
     function testSetOwner() public {
@@ -113,37 +192,123 @@ contract ClipperMomTest is DSTest {
 
     function testSetBreakerViaAuth() public {
         assertEq(clip.stopped(), 0);
-        caller.setBreaker(address(clip), 1);
+        caller.setBreaker(address(clip), 1, 0);
         assertEq(clip.stopped(), 1);
-        caller.setBreaker(address(clip), 2);
+        caller.setBreaker(address(clip), 2, 0);
         assertEq(clip.stopped(), 2);
-        caller.setBreaker(address(clip), 0);
+        caller.setBreaker(address(clip), 3, 0);
+        assertEq(clip.stopped(), 3);
+        caller.setBreaker(address(clip), 0, 0);
         assertEq(clip.stopped(), 0);
     }
 
     function testSetBreakerViaOwner() public {
         mom.setAuthority(address(0));
-        mom.setBreaker(address(clip), 1);
+        mom.setBreaker(address(clip), 1, 0);
         assertEq(clip.stopped(), 1);
-        mom.setBreaker(address(clip), 2);
+        mom.setBreaker(address(clip), 2, 0);
         assertEq(clip.stopped(), 2);
-        mom.setBreaker(address(clip), 0);
+        mom.setBreaker(address(clip), 3, 0);
+        assertEq(clip.stopped(), 3);
+        mom.setBreaker(address(clip), 0, 0);
         assertEq(clip.stopped(), 0);
     }
 
     function testFailSetBreakerNoAuthority() public {
         mom.setAuthority(address(0));
         assertTrue(mom.owner() != address(caller));
-        caller.setBreaker(address(clip), 1);
+        caller.setBreaker(address(clip), 1, 0);
     }
 
     function testFailSetBreakerUnauthorized() public {
         mom.setAuthority(address(new SimpleAuthority(address(this))));
         assertTrue(mom.owner() != address(caller));
-        caller.setBreaker(address(clip), 1);
+        caller.setBreaker(address(clip), 1, 0);
     }
 
     function testFailSetBreakerWrongLevel() public {
-        caller.setBreaker(address(clip), 3);
+        caller.setBreaker(address(clip), 4, 0);
+    }
+
+    function testFailSetToleranceViaAuth() public {
+        caller.setPriceTolerance(address(clip), 100);
+    }
+
+    function testSetToleranceViaOwner() public {
+        assertEq(mom.tolerance(address(clip)), 0);
+        mom.setPriceTolerance(address(clip), 100);
+        assertEq(mom.tolerance(address(clip)), 100);
+    }
+
+    function testTripBreaker() public {
+        assertEq(clip.stopped(), 0);
+        mom.setPriceTolerance(address(clip), 60 * RAY / 100); // max 40% drop
+        osm.setCurPrice(100 * WAD, 1);
+        osm.setNxtPrice(59 * WAD, 1);
+
+        anyone.tripBreaker(address(clip));
+        assertEq(clip.stopped(), 2);
+    }
+
+    function testTripBreaker2() public {
+        clip.file("stopped", 1);
+        assertEq(clip.stopped(), 1);
+        mom.setPriceTolerance(address(clip), 60 * RAY / 100); // max 40% drop
+        osm.setCurPrice(100 * WAD, 1);
+        osm.setNxtPrice(59 * WAD, 1);
+
+        anyone.tripBreaker(address(clip));
+        assertEq(clip.stopped(), 2);
+    }
+
+    function testFailTripBreakerAlreadyStopped() public {
+        clip.file("stopped", 2);
+        mom.setPriceTolerance(address(clip), 60 * RAY / 100); // max 40% drop
+        osm.setCurPrice(100 * WAD, 1);
+        osm.setNxtPrice(59 * WAD, 1);
+
+        anyone.tripBreaker(address(clip));
+    }
+
+    function testFailTripBreakerAlreadyStopped2() public {
+        clip.file("stopped", 3);
+        mom.setPriceTolerance(address(clip), 60 * RAY / 100); // max 40% drop
+        osm.setCurPrice(100 * WAD, 1);
+        osm.setNxtPrice(59 * WAD, 1);
+
+        anyone.tripBreaker(address(clip));
+    }
+
+    function testFailTripBreakerWithinBounds() public {
+        mom.setPriceTolerance(address(clip), 60 * RAY / 100);
+        osm.setCurPrice(100 * WAD, 1);
+        osm.setNxtPrice(60 * WAD, 1);
+
+        anyone.tripBreaker(address(clip));
+    }
+
+    function testTripBreakerLockedAndWait() public {
+        mom.setPriceTolerance(address(clip), 60 * RAY / 100);
+        osm.setCurPrice(100 * WAD, 1);
+        osm.setNxtPrice(59 * WAD, 1);
+
+        anyone.tripBreaker(address(clip));
+        assertEq(clip.stopped(), 2);
+        uint256 delay = 3600;
+        mom.setBreaker(address(clip), 0, delay);
+        assertEq(clip.stopped(), 0);
+        hevm.warp(block.timestamp + delay + 1);
+        anyone.tripBreaker(address(clip));
+        assertEq(clip.stopped(), 2);
+    }
+
+    function testFailTripBreakerLocked() public {
+        mom.setPriceTolerance(address(clip), 60 * RAY / 100);
+        osm.setCurPrice(100 * WAD, 1);
+        osm.setNxtPrice(59 * WAD, 1);
+
+        anyone.tripBreaker(address(clip));
+        mom.setBreaker(address(clip), 0, 3600);
+        anyone.tripBreaker(address(clip));
     }
 }

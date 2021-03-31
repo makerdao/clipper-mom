24f8a20eadb2f5dbc5f4dcba5dc872941fc3f9f9
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-03-31 15:22:42 -0300

    Remove unused safe sub

--- src/ClipperMom.sol
+++ src/ClipperMom.sol
@@ -73,9 +73,6 @@ contract ClipperMom {
     function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require((z = x + y) >= x);
     }
-    function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        require((z = x - y) <= x);
-    }
     function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require(y == 0 || (z = x * y) / y == x);
     }
